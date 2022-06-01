ba39bd74c2e7f57f72f74b1770e862ffa037c164
Frinhani <thiago.frinhani@ths.inf.br>
2022-06-01 17:10:35 -0300

    Tarefa 1: Praticando React

--- 1_INTRO/hello-world/src/App.js
+++ 1_INTRO/hello-world/src/App.js
@@ -7,6 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello World React!</p>
+
+        <div className="box">
+          <h1>Aula de React</h1>
+          <p>Aprendendo uma nova ferramenta de Java Script</p>
+        </div>
       </header>
     </div>
   );
