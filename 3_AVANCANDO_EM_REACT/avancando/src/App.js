9d60cf1d7903250a568f50dbc02f88ab6467ed5b
Frinhani <thiago.frinhani@ths.inf.br>
2022-06-02 11:43:37 -0300

    useState

---
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -0,0 +1,35 @@
+//components
+import ManageData from './components/ManageData';
+
+// style
+import './App.css';
+
+// imagem
+import City from "./assets/city.jpg";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avançando em React</h1>
+      
+      {/* Imagem em public */}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+      
+      {/*Imagem em assets */}
+      <div>
+        <img src={City} alt="City" />
+      </div>
+      
+      <div>
+        <ManageData />
+      </div>
+      
+
+    </div>
+  );
+}
+
+export default App;
