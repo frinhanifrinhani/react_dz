1693fa6f8a890d75b74843d2b7a22a35c6498b80
Frinhani <thiago.frinhani@ths.inf.br>
2022-06-04 11:29:24 -0300

    Fragment element

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -1,9 +1,4 @@
-import { useState } from 'react';
-
-//components
-import ManageData from './components/ManageData';
-import ListRender from './components/ListRender';
-import ConditionalRender from './components/ConditionalRender';
+import {  useState } from 'react';
 
 // style
 import './App.css';
@@ -13,10 +8,22 @@ import City from "./assets/city.jpg";
 import ShowUserName from './components/ShowUserName';
 import CarDetails from './components/CarDetails';
 
+//components
+import ManageData from './components/ManageData';
+import ListRender from './components/ListRender';
+import ConditionalRender from './components/ConditionalRender';
+import Fragment from './components/Fragment';
+
 function App() {
 
-  const name="Thiago Frinhani";
+  //const name="Thiago Frinhani";
   const [userName] = useState("Thiago");
+
+  const cars = [
+    {id: 1 , brand: "Ferrari", color: "Amarela", newCar: true, km: 0},
+    {id: 2, brand: "KIA", color: "Branco", newCar: false , km:  3400},
+    {id: 3, brand: "Renault", color: "Azul", newCar:  false, km: 2330}
+  ]
 
   return (
     <div className="App">
@@ -57,6 +64,14 @@ function App() {
       {/* reaproveitamento */} 
       <CarDetails brand="Ford" km={0} color="Vermelha" newCar={true} />
       <CarDetails brand="Ford" km={4500} color="Branco" newCar={false} />
+
+      {/* loop em array de objetos */} 
+      {cars.map((car) => (
+        <CarDetails brand={car.brand} color={car.color} km={car.km} newCar={car.newCar} />
+      ))}
+
+      {/* fragment */}
+      <Fragment titulo="Terceiro título" />
       
     </div>
   );
---
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/Fragment.js
@@ -0,0 +1,11 @@
+const Fragment = ({titulo}) => {
+  return (
+    <>
+        <h2>Primeiro título</h2>
+        <h3>Segundo título</h3>
+        <h4>{titulo}</h4>
+    </>
+  )
+}
+
+export default Fragment;
